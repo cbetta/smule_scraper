fcfb62fda35eff7ea2dc0d0b0048487b17359d19
Cristiano Betta <cbetta@gmail.com>
2014-10-22 17:33:55 +0100

    Adds environment variables

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,22 +1,24 @@
-require "capybara"
-require "capybara/dsl"
-require "capybara-webkit"
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara-webkit'
+require 'dotenv'
 
 class Scraper
   include Capybara::DSL
 
   def initialize
+    Dotenv.load
     Capybara.run_server = false
     Capybara.current_driver = :webkit
-    Capybara.app_host = "http://www.smule.com/"
+    Capybara.app_host = 'http://www.smule.com/'
   end
 
   def login
-    visit("/")
+    visit('/')
     click_on('Log In')
     click_on('Login')
-    find(:css, ".snp-modal-form-username").set("paypal14")
-    find(:css, ".snp-modal-form-password").set("paypal14")
+    find(:css, '.snp-modal-form-username').set(ENV["USERNAME"])
+    find(:css, '.snp-modal-form-password').set(ENV["PASSWORD"])
     click_on('Login')
     sleep(5)
     page.html
